9af8fed14f4ba4b17584971e3319f211724c35e4
AdithyasacharyaWorks <156585313+AdithyasacharyaWorks@users.noreply.github.com>
2024-07-03 13:03:59 +0530

    altered meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Hotels in Kundapur",
-  description: "Best hotels in Kundapur udupi, kundapur ,best in kundapur , samyudutha , cheap hotel , best value for money Get Amazing deals on Budget Hotels in Kundapur.✔ Use coupon code &amp; Get Upto 70% OFF instantly on Kundapur Hotels.Cheap hotels in Kundapur with tariff, amenities &amp; all details. Find budget room rates, location, address, contact phone no and  more.",
-  keywords:"Budget, Cheap, Budget Kundapur hotels, Kundapur, Budget hotel deals in Kundapur, Budget Kundapur hotel offers, Kundapur Budget hotels, Kundapur hotels, budget accommodation in Kundapur, budget accommodation Kundapur, budget travel Kundapur, Cheap Hotels in Kundapur"
+  description: "Best hotels in Kundapur udupi, kundapur ,best in kundapur , samyudutha , cheap hotel , best value for money Get Amazing deals on Budget Hotels in Kundapur.✔ Use coupon code &amp; Get Upto 70% OFF instantly on Kundapur Hotels.Cheap hotels in Kundapur with tariff, amenities &amp; all details. Find budget room rates, location, address, contact phone no and  more kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapurkundapurkundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapurkundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur ",
+  keywords:"Budget, Cheap, Budget Kundapur hotels, Kundapur, Budget hotel deals in Kundapur, Budget Kundapur hotel offers, Kundapur Budget hotels, Kundapur hotels, budget accommodation in Kundapur, budget accommodation Kundapur, budget travel Kundapur, Cheap Hotels in Kundapur  , Kundapur, Kundapur , Kundapur , Kundapur, Kundapur"
 
 };
 
