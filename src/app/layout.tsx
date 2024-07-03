f2d3601e7f3c7d10eb2f52e0c965a1710f76c86a
AdithyasacharyaWorks <156585313+AdithyasacharyaWorks@users.noreply.github.com>
2024-07-03 23:26:39 +0530

    added some meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Hotels in Kundapur, samudyutha",
-  description: "samudyutha , samudyutha inn ,Best hotels in Kundapur udupi, kundapur ,best in kundapur , samyudutha , cheap hotel , best value for money Get Amazing deals on Budget Hotels in Kundapur.✔ Use coupon code &amp; Get Upto 70% OFF instantly on Kundapur Hotels.Cheap hotels in Kundapur with tariff, amenities &amp; all details. Find budget room rates, location, address, contact phone no and  more kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapurkundapurkundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapurkundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur ",
-  keywords:" samudyutha ,samudyutha inn , sam , samud , samudyatha , Budget, Cheap, Budget Kundapur hotels, Kundapur, Budget hotel deals in Kundapur, Budget Kundapur hotel offers, Kundapur Budget hotels, Kundapur hotels, budget accommodation in Kundapur, budget accommodation Kundapur, budget travel Kundapur, Cheap Hotels in Kundapur  , Kundapur, Kundapur , Kundapur , Kundapur, Kundapur"
+  title: "Taste of Karavali",
+  description: " taste of karavali samudyutha , samudyutha inn ,Best hotels in Kundapur udupi, kundapur ,best in kundapur , samyudutha , cheap hotel , best value for money Get Amazing deals on Budget Hotels in Kundapur.✔ Use coupon code &amp; Get Upto 70% OFF instantly on Kundapur Hotels.Cheap hotels in Kundapur with tariff, amenities &amp; all details. Find budget room rates, location, address, contact phone no and  more kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapurkundapurkundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapurkundapur kundapur kundapur kundapur kundapur kundapur kundapur kundapur ",
+  keywords:" taste of karavali,tasteofkaravali,taste,karavali,of,samudyutha ,samudyutha inn , sam , samud , samudyatha , Budget, Cheap, Budget Kundapur hotels, Kundapur, Budget hotel deals in Kundapur, Budget Kundapur hotel offers, Kundapur Budget hotels, Kundapur hotels, budget accommodation in Kundapur, budget accommodation Kundapur, budget travel Kundapur, Cheap Hotels in Kundapur  , Kundapur, Kundapur , Kundapur , Kundapur, Kundapur"
 
 };
 
